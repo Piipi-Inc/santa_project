09418c4ea7066dedeb81ceb571a110c3cc663828
Lyosha Fatin <70691537+leshafatin@users.noreply.github.com>
2024-12-07 19:31:22 +0300

    add adaptive (#2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
-import "./App.css";
+import { useLayoutEffect } from "react";
+import { setupFontSize } from "./shared/libs/setupFontSize";
 
 export const App = () => {
-  return <div></div>;
+  useLayoutEffect(() => {
+    const setupUi = () => {
+      setTimeout(() => {
+        setupFontSize();
+      }, 20);
+    };
+
+    setupUi();
+
+    window.addEventListener("resize", setupUi);
+
+    return () => {
+      window.removeEventListener("resize", setupUi);
+    };
+  });
+
+  return (
+    <div>
+      <h2 style={{ fontSize: "12rem" }}>пук пук как как </h2>
+    </div>
+  );
 };
